83b9db13751e93c1d08868cfa3e06a4425b9e749
dndqlszzz <mandarin1999@naver.com>
2023-05-27 00:26:28 +0900

    변경사항. 1. RegisterActivity.kt 1-1. 닉네임 중복확인 기능 구현 1-3. 기입한 정보들은 firestorm에 저장됨
    2. activity_register.xml
    2-1. 주민등록번호 -> 생년월일 및 성별 로 변경
    
    3. Firestorm
    <규칙>란을 보면 코드가 있는데
    읽기쓰기 모두 거부 -> 모든 사용자가 읽기, 쓰기 허용 으로 변경  => 회원가입할때 닉네임 중복여부 확인을 위해 읽기 허용, 유저 정보 등록을 위해 쓰기 허용

---
+++ app/src/main/java/com/example/tukxi/FindActivity.kt
@@ -0,0 +1,9 @@
+package com.example.tukxi
+
+import androidx.appcompat.app.AppCompatActivity
+
+class FindActivity :AppCompatActivity(){
+
+
+
+}
--- app/src/main/java/com/example/tukxi/RegisterActivity.kt
+++ app/src/main/java/com/example/tukxi/RegisterActivity.kt
@@ -1,37 +1,75 @@
 package com.example.tukxi
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Nickname
+import android.provider.MediaStore.Audio.Radio
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.ktx.actionCodeSettings
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.auth.FirebaseUser as FirebaseUser
 
 class RegisterActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
     private lateinit var email: EditText
     private lateinit var password: EditText
     private lateinit var password_Confirm: EditText
+    private lateinit var name : EditText
+    private lateinit var nickname: EditText
+    private lateinit var accountNum : EditText
+    private lateinit var phoneNum : EditText
+    private lateinit var birth : EditText
+
+    private lateinit var radiogroup_gender : RadioGroup
+    private lateinit var btnMale : RadioButton
+    private lateinit var btnFemale : RadioButton
+
     private lateinit var btnRegister: Button
-    private lateinit var btnCheck: Button
+    private lateinit var btnEmailCheck: Button
+    private lateinit var btnNicknameCheck : Button
+
+    private lateinit var auth: FirebaseAuth
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        //EditText 변수들
         email = findViewById(R.id.et_regi_email)
         password = findViewById(R.id.et_regi_pwd)
         password_Confirm = findViewById(R.id.et_regi_pwdConfirm)
-        btnRegister = findViewById(R.id.btn_RegisterFinish)
-        btnCheck = findViewById(R.id.btn_checkEmail)
-        auth = FirebaseAuth.getInstance()
-        var idCheck = 0
+        name = findViewById(R.id.et_AccountNum)
+        nickname = findViewById(R.id.et_Nickname)
+        accountNum = findViewById(R.id.et_AccountNum)
+        birth = findViewById(R.id.et_Birth)
+        phoneNum = findViewById(R.id.et_PhoneNum)
 
-        btnCheck.setOnClickListener {
+        //Button 변수들
+        btnRegister = findViewById(R.id.btn_RegisterFinish)
+        btnEmailCheck = findViewById(R.id.btn_checkEmail)
+        btnMale = findViewById(R.id.btnGenderMale)
+        btnFemale = findViewById(R.id.btnGenderFemale)
+        btnNicknameCheck = findViewById(R.id.btn_NicknameCheck)
+        radiogroup_gender = findViewById(R.id.rdg_gender)
+        //Firebase 변수
+        auth = FirebaseAuth.getInstance()
+
+        // 기타 변수
+        var idCheck : Boolean = false
+        var Gender : String = "None"
+        var NicknameCheck : Boolean = false
+
+        // 이메일 중복 검사 버튼
+        btnEmailCheck.setOnClickListener {
             val email: String = email.text.toString()
             auth.fetchSignInMethodsForEmail(email)
                 .addOnCompleteListener { task ->
@@ -44,7 +82,7 @@ class RegisterActivity : AppCompatActivity() {
                         } else {
                             // 이메일 중복 검사 통과
                             Toast.makeText(this,"해당 이메일은 사용 가능합니다.",Toast.LENGTH_SHORT).show()
-                            idCheck = 1
+                            idCheck = true
                         }
                     } else {
                         // 이메일 중복 검사 실패
@@ -52,13 +90,45 @@ class RegisterActivity : AppCompatActivity() {
                     }
                 }
         }
+        //닉네임 중복 검사 버튼
+        btnNicknameCheck.setOnClickListener{
+            val nickname : String = nickname.text.toString()
 
+            checkNickNameAvailability(nickname){isAvailable->
+                if(isAvailable){
+                    NicknameCheck = true
+                    Toast.makeText(this, "사용 가능한 닉네임입니다.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "이미 존재하는 닉네임입니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+
+
+        // 성별 라디오버튼
+        radiogroup_gender.setOnCheckedChangeListener{group,checkedGender->
+            when(checkedGender){
+                R.id.btnGenderMale->{
+                    Gender = "Male"
+                }
+                R.id.btnGenderFemale->{
+                    Gender = "Female"
+                }
+            }
+        }
+        // 회원가입 버튼 -> 유저 정보 저장 및 해당 이메일로 인증 메일 전송
         btnRegister.setOnClickListener {
             val email = email.text.toString()
             val password = password.text.toString()
             val passwordConfirm = password_Confirm.text.toString()
+            val name = name.text.toString()
+            val nickname = nickname.text.toString()
+            val accountNum = accountNum.text.toString()
+            val birth = birth.text.toString()
+            val phoneNum = phoneNum.text.toString()
 
-            if(idCheck != 1){
+                if(!idCheck){
                 Toast.makeText(this,"중복된 이메일.",Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
@@ -66,11 +136,18 @@ class RegisterActivity : AppCompatActivity() {
                 Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+            if (!NicknameCheck){
+                Toast.makeText(this,"중복된 닉네임",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
             else{
+                saveUserData(email,password,name,nickname,accountNum,phoneNum,birth,Gender)
                 createUserWithEmailAndPassword(email,password)
             }
         }
     }
+
+            // 회원가입 기능 (FirebaseAuth에 유저 등록)
             private fun createUserWithEmailAndPassword(email: String, password: String) {
                 auth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this) { task ->
@@ -108,4 +185,34 @@ class RegisterActivity : AppCompatActivity() {
                         }
                     }
             }
-        }
+
+    // 회원 정보 저장
+    fun saveUserData(Email : String, Password: String, Name : String, Nickname : String, AccountNum : String, PhoneNum : String,Birth : String,Gender : String){
+        val db = FirebaseFirestore.getInstance()
+        val user = hashMapOf(
+            "Email" to Email,
+            "Password" to Password,
+            "Name" to Name,
+            "Nickname" to Nickname,
+            "AccountNum" to AccountNum,
+            "PhoneNum" to PhoneNum,
+            "Birth" to Birth,
+            "Gender" to Gender)
+        db.collection("UserInformation")// "UserInformation" 데이터베이스에 저장
+            .add(user)
+    }
+    // 닉네임 중복 검사
+    fun checkNickNameAvailability(Nickname: String, callback:(Boolean)->Unit){
+        val db = FirebaseFirestore.getInstance()
+        db.collection("UserInformation")
+            .whereEqualTo("Nickname",Nickname)
+            .get()
+            .addOnSuccessListener{querySnapshot->
+                val isAvailable = querySnapshot.isEmpty
+                callback(isAvailable)
+            }
+            .addOnFailureListener{ e->
+                callback(false)
+            }
+    }
+}
