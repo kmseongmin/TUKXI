f8fd03abf56e7b79eff96fedf04fd2a34727c91c
kimseongmin <newsungk7@naver.com>
2023-05-26 22:12:17 +0900

    seongmin

--- app/src/main/java/com/example/tukxi/MapActivity.kt
+++ app/src/main/java/com/example/tukxi/MapActivity.kt
@@ -1,19 +1,27 @@
 package com.example.tukxi
 
 
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import com.example.tukxi.databinding.ActivityMapBinding
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MapActivity : Fragment() {
+class MapActivity : Fragment(), OnMapReadyCallback{
     private var _binding: ActivityMapBinding? = null
     private val binding get() = _binding!!
+    private lateinit var mapView : MapView
+    var mLocationManager: LocationManager? = null
+    var mLocationListener: LocationListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
@@ -25,18 +33,51 @@ class MapActivity : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = ActivityMapBinding.inflate(inflater, container, false)
-        val view = binding.root
 
-        return view
+        mapView = binding.mapFragment as MapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) { // 프래그먼트가 실행 된 이후에 보일 화면
         super.onViewCreated(view, savedInstanceState)
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
     }
 }
 
