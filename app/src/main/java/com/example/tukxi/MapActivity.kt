87b4b3975297bd6b8b21b41ee28881bb2a908ba9
kimseongmin <newsungk7@naver.com>
2023-05-27 21:31:56 +0900

    seongmin

--- app/src/main/java/com/example/tukxi/MapActivity.kt
+++ app/src/main/java/com/example/tukxi/MapActivity.kt
@@ -1,15 +1,19 @@
 package com.example.tukxi
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.tukxi.databinding.ActivityMapBinding
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
@@ -22,6 +26,11 @@ class MapActivity : Fragment(), OnMapReadyCallback {
     private val binding get() = _binding!!
 
     private lateinit var mapView: MapView
+    private lateinit var edtStart: EditText
+    private lateinit var edtFinish: EditText
+    private lateinit var quickMatchingbtn: Button
+    private lateinit var searchRoombtn: Button
+    private lateinit var makeRoombtn: Button
     private lateinit var googleMap: GoogleMap
     private val LOCATION_PERMISSION_REQUEST_CODE = 1
 
@@ -31,7 +40,13 @@ class MapActivity : Fragment(), OnMapReadyCallback {
     ): View? {
         _binding = ActivityMapBinding.inflate(inflater, container, false)
 
-        mapView = binding.mapFragment as MapView
+        edtStart = binding.editTextStart
+        edtFinish = binding.editTextFinish
+        quickMatchingbtn = binding.quickMatchingbtn
+        searchRoombtn = binding.searchRoombtn
+        makeRoombtn = binding.makeRoombtn
+        mapView = binding.mapFragment
+
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
 
@@ -40,29 +55,35 @@ class MapActivity : Fragment(), OnMapReadyCallback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        checkLocationPermission()
     }
 
     private fun checkLocationPermission() {
-        if (ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
         ) {
-            getCurrentLocation()
-        } else {
             ActivityCompat.requestPermissions(
                 requireActivity(),
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                 LOCATION_PERMISSION_REQUEST_CODE
             )
+        } else {
+            // 권한이 허용되었거나 이미 허용된 경우
+            googleMap.isMyLocationEnabled = true // 위치 정보 활성화
         }
     }
 
+    @SuppressLint("MissingPermission")
     private fun getCurrentLocation() {
-        val currentLocation = LatLng(37.123, 127.456)
-        googleMap.addMarker(MarkerOptions().position(currentLocation).title("현재 위치"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f))
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            location?.let {
+                val currentLocation = LatLng(location.latitude, location.longitude)
+                googleMap.let {
+                    it.addMarker(MarkerOptions().position(currentLocation).title("현재 위치"))
+                    it.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f))
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
@@ -70,24 +91,10 @@ class MapActivity : Fragment(), OnMapReadyCallback {
         _binding = null
     }
 
-    override fun onMapReady(p0: GoogleMap) {
-        p0?.let {
-            googleMap = it
-            if (ContextCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                googleMap.isMyLocationEnabled = true
-                getCurrentLocation()
-            } else {
-                ActivityCompat.requestPermissions(
-                    requireActivity(),
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    LOCATION_PERMISSION_REQUEST_CODE
-                )
-            }
-        }
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        checkLocationPermission()
+        getCurrentLocation()
     }
 
     override fun onStart() {
