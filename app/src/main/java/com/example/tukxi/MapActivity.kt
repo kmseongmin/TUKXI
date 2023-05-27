20c6f4475224b7c17d0546b5721e119db8d2f48a
kimseongmin <newsungk7@naver.com>
2023-05-27 17:53:46 +0900

    seongmin

--- app/src/main/java/com/example/tukxi/MapActivity.kt
+++ app/src/main/java/com/example/tukxi/MapActivity.kt
@@ -1,35 +1,32 @@
 package com.example.tukxi
 
-
-import android.location.LocationManager
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.tukxi.databinding.ActivityMapBinding
-import com.google.android.gms.location.LocationListener
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-
-class MapActivity : Fragment(), OnMapReadyCallback{
+class MapActivity : Fragment(), OnMapReadyCallback {
     private var _binding: ActivityMapBinding? = null
     private val binding get() = _binding!!
-    private lateinit var mapView : MapView
-    var mLocationManager: LocationManager? = null
-    var mLocationListener: LocationListener? = null
 
-    override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = ActivityMapBinding.inflate(inflater, container, false)
@@ -41,9 +38,31 @@ class MapActivity : Fragment(), OnMapReadyCallback{
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) { // 프래그먼트가 실행 된 이후에 보일 화면
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        checkLocationPermission()
+    }
 
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            getCurrentLocation()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun getCurrentLocation() {
+        val currentLocation = LatLng(37.123, 127.456)
+        googleMap.addMarker(MarkerOptions().position(currentLocation).title("현재 위치"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f))
     }
 
     override fun onDestroyView() {
@@ -52,32 +71,52 @@ class MapActivity : Fragment(), OnMapReadyCallback{
     }
 
     override fun onMapReady(p0: GoogleMap) {
-
+        p0?.let {
+            googleMap = it
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                googleMap.isMyLocationEnabled = true
+                getCurrentLocation()
+            } else {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_PERMISSION_REQUEST_CODE
+                )
+            }
+        }
     }
 
     override fun onStart() {
         super.onStart()
         mapView.onStart()
     }
+
     override fun onStop() {
         super.onStop()
         mapView.onStop()
     }
+
     override fun onResume() {
         super.onResume()
         mapView.onResume()
     }
+
     override fun onPause() {
         super.onPause()
         mapView.onPause()
     }
+
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
     }
+
     override fun onDestroy() {
         mapView.onDestroy()
         super.onDestroy()
     }
 }
-
