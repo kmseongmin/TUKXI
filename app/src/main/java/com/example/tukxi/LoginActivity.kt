8fa5802fd6b2386d40126dd0b9c663418747ce1d
dndqlszzz <mandarin1999@naver.com>
2023-05-28 19:46:55 +0900

    변경사항. 1. RegisterActivity.kt - 은행 선택 기능 구현
    2. activity_register.xml
    - 은행 선택 버튼 생성
    
    3. LoginActivity.kt
    1) 비밀번호 찾기 기능 구현
    1-1) 이메일을 입력했을 때 해당 이메일이 firebaseAuth에 존재하면 재설정 링크 보냄 존재하지 않으면 안보냄.
    
    4) Etc
    - xml 파일들 추가. (dialog 창 디자인)

--- app/src/main/java/com/example/tukxi/LoginActivity.kt
+++ app/src/main/java/com/example/tukxi/LoginActivity.kt
@@ -4,15 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var loginBtn: Button
     private lateinit var registerBtn: Button
+
     private lateinit var emailEt: EditText
     private lateinit var passwordEt: EditText
+
+    private lateinit var findPwdTv : TextView
+
     private lateinit var firebaseAuth: FirebaseAuth
 
 
@@ -21,13 +27,18 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        firebaseAuth = FirebaseAuth.getInstance()
+        findPwdTv = findViewById(R.id.findPassword)
+        // 버튼
         loginBtn = findViewById(R.id.btnLogin)
         registerBtn = findViewById(R.id.btnRegister)
+
+        // EditText
         emailEt = findViewById(R.id.et_email)
         passwordEt = findViewById(R.id.et_password)
+        // Firebase
+        firebaseAuth = FirebaseAuth.getInstance()
 
-
+        //Login 버튼 눌렀을 때.
         loginBtn.setOnClickListener {
             val email = emailEt.text.toString()
             val password = passwordEt.text.toString()
@@ -53,10 +64,65 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
         }
+        //회원가입 버튼 눌렀을 때
         registerBtn.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        //비밀번호 찾기 버튼 눌렀을 때
+        findPwdTv.setOnClickListener {
+            val builder = AlertDialog.Builder(this,R.style.RoundDialog)
+            builder.setTitle("비밀번호 찾기")
+            builder.setIcon(R.drawable.taxi1_icon)
+
+
+            val inputEmail = EditText(this)
+            inputEmail.hint = "이메일 입력"
+            builder.setView(inputEmail)
+
+            builder.setPositiveButton("확인") { dialog, _ ->
+                val email = inputEmail.text.toString().trim()
+                if (email.isNotEmpty()) {
+                    firebaseAuth.fetchSignInMethodsForEmail(email)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val signInMethods = task.result?.signInMethods
+                                if (signInMethods.isNullOrEmpty()) {
+                                    Toast.makeText(this, "존재하지 않는 이메일입니다.", Toast.LENGTH_SHORT).show()
+                                } else {
+                                    firebaseAuth.sendPasswordResetEmail(email)
+                                        .addOnCompleteListener { resetTask ->
+                                            if (resetTask.isSuccessful) {
+                                                Toast.makeText(
+                                                    this,
+                                                    "입력하신 이메일로 비밀번호 재설정 이메일을 보냈습니다.",
+                                                    Toast.LENGTH_SHORT
+                                                ).show()
+                                            } else {
+                                                Toast.makeText(
+                                                    this,
+                                                    "비밀번호 재설정 이메일을 보내는데 실패했습니다.",
+                                                    Toast.LENGTH_SHORT
+                                                ).show()
+                                            }
+                                        }
+                                }
+                            } else {
+                                Toast.makeText(this, "이메일 확인 중 오류가 발생하였습니다.", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                } else {
+                    Toast.makeText(this, "이메일을 입력하여 주세요.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            builder.setNegativeButton("취소") { dialog, _ ->
+                dialog.dismiss()
+            }
+            val dialog = builder.create()
+            dialog.show()
+        }
+
 
     }
 }
